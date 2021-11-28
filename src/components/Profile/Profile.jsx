5a78d9ce031c8a453cb5baa7c6f274fd18cc195e
Arsen Temurian <arsentim2611@gmail.com>
2021-11-28 21:32:47 +0200

    fixed bags

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -35,7 +35,12 @@ Profile.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  stats: PropTypes.object.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+
   avatar: PropTypes.string.isRequired,
 };
 
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -31,6 +31,9 @@ TransactionHistory.propTypes = {
   items: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
     })
   ),
 };
